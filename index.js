014fa0d2cc271f951d11fd33f17a9397a4cfd43a
mohamedsane <sanem9206@gmail.com>
2024-11-13 01:59:55 +0000

    update the app
    the end, app done

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ let screenElements = document.getElementById("calculator-display").children;
 let displayFirstItem = screenElements[0]
 let displayOperator = screenElements[1]
 let displaySecondItem = screenElements[2]
-let firstItem = "";//1er opérande
-let secondItem = "";//2eme opérande
-let currentOperator = null;//l'Opérateur du calcul actuel
-let isSecondItem = false;//Savoir si le deuxième nombre est entrain d'être entré
+let firstItem = "";//1st operand
+let secondItem = "";//2nd operand
+let currentOperator = null;//the current calculation operator
+let isSecondItem = false;//Know if the second number is being entered
 let isACPressed = false;
 let holdTimer;
 displayFirstItem.textContent = "";
@@ -30,8 +30,6 @@ let isSecondItemSignChanged = false;
 
 //to see on the display the buttons pressed
 function displayNumbersOnScreen(value) {
-    // displayValue += value;
-    // screenDisplay.textContent += value;
     if (!isSecondItem) {
         firstItem += value;
         displayFirstItem.textContent += value;
@@ -40,28 +38,27 @@ function displayNumbersOnScreen(value) {
         displaySecondItem.textContent += value
     }
 }
+
 function displayOperatorsOnScreen(operator) {
     
     if ( operator === "-" && firstItem === "-")  {
-        console.log(`%c🎨 ⍨ display operator wait`, "Your_CSS_Goes_Here");
         displayOperator.textContent = "";
-    } else if ((operator === "+" || operator === "x" || operator === "÷" ) && firstItem === "") {
-        console.log(firstItem)
-        console.log(`%c🎨 ⍨ operator + ou x ou /`, "Your_CSS_Goes_Here");
+    } else if (
+        (operator === "+" || operator === "*" || operator === "/" ) 
+        && 
+        firstItem === "") {
         displayOperator.textContent = "";
     } else if (operator) {
         displayOperator.textContent = operator;
     }
 }
 
-//to be able to click on the buttons
+//to be able to click on the digit buttons
 function iteration(button) {
 
     for(let i = 0; i < button.length; i++ ){
         button[i].addEventListener("click", (event) => {   
             displayNumbersOnScreen(event.target.value)
-            console.log(`%c🎨 ⍨ ${firstItem}`, "font-size:30px;color:red");
-            console.log(`%c🎨 ⍨ ${secondItem}`, "font-size:30px;color:blue")
         });
     }
 }
@@ -77,37 +74,44 @@ function calculate() {
     if (firstItem === ".") return;
     if (firstItem.endsWith(minusSign)) return;
     
-    // Convertir les valeurs en nombres
+    // Convert values to numbers
     let num1 = parseFloat(firstItem);
     let num2 = parseFloat(secondItem);
 
-    if (displayFirstItem.textContent.includes("(-" + firstItem + ")") && !displaySecondItem.textContent.includes("(-" + secondItem + ")")) {
+    if (displayFirstItem.textContent.includes("(-" + firstItem + ")") 
+        && 
+        !displaySecondItem.textContent.includes("(-" + secondItem + ")")) {
         num1 = (-num1);
-        console.log(`%c🎨 ⍨ num1`, "color:yellowgreen; font-weight:bold", displayFirstItem.textContent);
-    } else if (displaySecondItem.textContent.includes("(-" + secondItem + ")") && !displayFirstItem.textContent.includes("(-" + firstItem + ")")) {
+    } else if (displaySecondItem.textContent.includes("(-" + secondItem + ")") 
+        && 
+        !displayFirstItem.textContent.includes("(-" + firstItem + ")")) {
         num2 = (-num2);
-        console.log(`%c🎨 ⍨ num2`, "color:green; font-weight:bold", num2);
-    } else if (displayFirstItem.textContent.includes("(-" + firstItem + ")") && displaySecondItem.textContent.includes("(-" + secondItem + ")")) {
+    } else if (displayFirstItem.textContent.includes("(-" + firstItem + ")") 
+        && 
+        displaySecondItem.textContent.includes("(-" + secondItem + ")")) {
         num1 = (-num1);       
         num2 = (-num2);
-        console.log(`%c🎨 ⍨ displayFirstItem.textContent`, "color:pink; font-weight:bold", displayFirstItem.textContent);
-        console.log(`%c🎨 ⍨ displaySecondItem.textContent`, "color:pink; font-weight:bold", displaySecondItem.textContent);
     }
 
-    if (displayFirstItem.textContent.includes("%") && !displaySecondItem.textContent.includes("%")) {
-        let percentage = 1/100;
-        num1 *= percentage;
-        console.log(num1);
-    } else if (displaySecondItem.textContent.includes("%") && !displayFirstItem.textContent.includes("%") ) {
-        let percentage = 1/100;
-        num2 *= percentage;
-    } else if (displayFirstItem.textContent.includes("%") && displaySecondItem.textContent.includes("%")) {
-        let percentage = 1/100;
-        num1 *= percentage;
-        num2 *= percentage;
+    if (displayFirstItem.textContent.includes("%") 
+        && 
+        !displaySecondItem.textContent.includes("%")) {
+            let percentage = 1/100;
+            num1 *= percentage;
+    } else if (displaySecondItem.textContent.includes("%") 
+        && 
+        !displayFirstItem.textContent.includes("%")) {
+            let percentage = 1/100;
+            num2 *= percentage;
+    } else if (displayFirstItem.textContent.includes("%") 
+        && 
+        displaySecondItem.textContent.includes("%")) {
+            let percentage = 1/100;
+            num1 *= percentage;
+            num2 *= percentage;
     }
 
-    // Appeler la fonction operate
+    // Call the operate function
     let result;
     if (firstItem.includes(".") || secondItem.includes(".")) {
         result = operate(num1, num2, currentOperator).toFixed(2);
@@ -117,14 +121,14 @@ function calculate() {
         result = operate(num1, num2, currentOperator)
     }
 
-    // Afficher le résultat
+    // Display result
     displayFirstItem.textContent = result;
     displayOperator.textContent = "";
     displaySecondItem.textContent = "";
 
 
-    // Réinitialiser les variables
-    firstItem = result.toString();  // Le résultat devient le premier nombre pour de futures opérations
+    // Reset variables
+    firstItem = result.toString();  // The result becomes the first number for future operations
     secondItem = "";
     currentOperator = null;
     isSecondItem = false;
@@ -145,32 +149,24 @@ function basculateToSecondItem() {
                 iteration.addEventListener("click", (event) => {
                     currentOperator = addOperation();
                     displayOperatorsOnScreen(event.target.value)
-                    console.log(`%c🎨 ⍨ success`, "color:purple");
-                    console.log(`%c🎨 ⍨ ${isSecondItem}`, "color:orange")
                 });
                 break;
             case "-" :
                 iteration.addEventListener("click", (event) => {
                     currentOperator = minusOperation();
                     displayOperatorsOnScreen(event.target.value)
-                    console.log(`%c🎨 ⍨ success`, "color:purple");
-                    console.log(`%c🎨 ⍨ ${isSecondItem}`, "color:orange")
                 });
                 break;
-            case "x":
+            case "*":
                 iteration.addEventListener("click", (event) => {
                     currentOperator = multiplyOperation();
                     displayOperatorsOnScreen(event.target.value)
-                    console.log(`%c🎨 ⍨ success`, "color:purple");
-                    console.log(`%c🎨 ⍨ ${isSecondItem}`, "color:orange")
                 });
                 break;
-            case "÷" :
+            case "/" :
                 iteration.addEventListener("click", (event) => {
                     currentOperator = divideOperation();
                     displayOperatorsOnScreen(event.target.value)
-                    console.log(`%c🎨 ⍨ success`, "color:purple");
-                    console.log(`%c🎨 ⍨ ${isSecondItem}`, "color:orange")
                 });
                 break;
             case "" :
@@ -197,23 +193,23 @@ function addCommaToSecondItem(comma) {
 function restrictionToCommas() {
 
     if (!isSecondItem) {
-            //verifier le first item en 1er
-    if (firstItem.includes(".") || displayFirstItem.textContent.includes("(-" + firstItem + ")")) {
-        console.log(`%c🎨 ⍨ point`, "Your_CSS_Goes_Here");
-        // commaButton.removeEventListener("click", restrictionToCommas);
+        //check the first item in 1st
+        if (firstItem.includes(".") 
+            || 
+            displayFirstItem.textContent.includes("(-" + firstItem + ")")) {
+                return
+        } else {
+            addCommaToFirstItem(".")
+        }
     } else {
-        addCommaToFirstItem(".")
-        console.log(`%c🎨 ⍨ no point`, "Your_CSS_Goes_Here");
-    }
-    } else {
-            //passer à la vérification de second item en second lieu
-    if (secondItem.includes(".") || displaySecondItem.textContent.includes("(-" + secondItem + ")")) {
-        console.log(`%c🎨 ⍨ point`, "Your_CSS_Goes_Here");
-        // commaButton.removeEventListener("click", restrictionToCommas);
-    } else {
-        addCommaToSecondItem(".")
-        console.log(`%c🎨 ⍨ no point`, "Your_CSS_Goes_Here");
-    }
+            //pass to second item verification in second place
+        if (secondItem.includes(".") 
+            || 
+            displaySecondItem.textContent.includes("(-" + secondItem + ")")) {
+                return 
+        } else {
+            addCommaToSecondItem(".")
+        }
     }
 
 }
@@ -255,16 +251,9 @@ acButton.addEventListener("mouseleave", () => {
 
 function clearOnce() {
 
-    let secondItemLength = secondItem.length;
-    let firstItemLength = firstItem.length;
-    // if (secondItem) {
-    //     console.log(`%c🎨 ⍨ secondItem digit erased`, "Your_CSS_Goes_Here")
-    //     // secondItem = secondItem.slice(0, -1);
-    //     secondItem = secondItem.replace(secondItem[secondItemLength - 1], "");
-    //     displaySecondItem.textContent = secondItem;
-    // }
     if((beAdd || beMinus || beDivide || beMultiply) && secondItem) {
-        secondItem = secondItem.replace(secondItem[secondItemLength - 1], "");
+        // secondItem = secondItem.replace(secondItem[secondItemLength - 1], "");
+        secondItem = secondItem.slice(0, -1);
         displaySecondItem.textContent = secondItem;
     } else if((beAdd || beMinus || beMultiply || beDivide) && secondItem === "") {
         console.log("operator erased");
@@ -274,12 +263,14 @@ function clearOnce() {
         beMinus = false;
         isSecondItem = false;
         displayOperatorsOnScreen(" ");
-    } else if (firstItem && (!beAdd || !beMinus || !beMultiply || !beDivide) && secondItem === "") {
-        firstItem = firstItem.replace(firstItem[firstItemLength - 1], "");
-        displayFirstItem.textContent = firstItem;
-        console.log("firstItem erased");
-        isFirstItemSignChanged = false
-    }
+    } else if (firstItem && (!beAdd || !beMinus || !beMultiply || !beDivide) 
+                && 
+                secondItem === "") {
+                    firstItem = firstItem.slice(0, -1);
+                    displayFirstItem.textContent = firstItem;
+                    console.log("firstItem erased");
+                    isFirstItemSignChanged = false
+                }
 }
 
 acButton.addEventListener("click", () => clearOnce());
@@ -295,22 +286,18 @@ function addPercentageToSecondItem(percentage) {
 
 function restrictionToPercentages() {
     if (!isSecondItem) {
-        //verifier le first item en 1er
+        //check firstItem first
         if (displayFirstItem.textContent.includes("%")) {
-        console.log(`%c🎨 ⍨ percentage`, "Your_CSS_Goes_Here");
-        // commaButton.removeEventListener("click", restrictionToCommas);
-    } else {
+            return
+        } else {
         addPercentageToFirstItem("%");
-        console.log(`%c🎨 ⍨ no percentage`, "Your_CSS_Goes_Here");
-    }
+        }
     } else {
-        //passer à la vérification de second item en second lieu
+        //then check secondItem
         if (displaySecondItem.textContent.includes("%")) {
-        console.log(`%c🎨 ⍨ percentage`, "Your_CSS_Goes_Here");
-        // commaButton.removeEventListener("click", restrictionToCommas);
-    } else {
+            return
+        } else {
         addPercentageToSecondItem("%");
-        console.log(`%c🎨 ⍨ no percentage`, "Your_CSS_Goes_Here");
         }
     }
 }
@@ -318,38 +305,39 @@ function restrictionToPercentages() {
 calculatorButtons.aczpercentage[1].addEventListener("click", restrictionToPercentages);
 
 function changingSignOfFirstItem() {
-    isFirstItemSignChanged ? displayFirstItem.textContent = "(-" + firstItem + ")" : displayFirstItem.textContent = firstItem;
+    isFirstItemSignChanged ?
+    displayFirstItem.textContent = "(-" + firstItem + ")" 
+    : 
+    displayFirstItem.textContent = firstItem;
 }
 function changingSignOfSecondItem() {
     
-    isSecondItemSignChanged ? displaySecondItem.textContent = "(-" + secondItem + ")" : displaySecondItem.textContent = secondItem;
+    isSecondItemSignChanged ? 
+    displaySecondItem.textContent = "(-" + secondItem + ")" 
+    : 
+    displaySecondItem.textContent = secondItem;
 }
 function restrictionToChangeSign() {
 
 
     if (!isSecondItem) {
-        //verifier le first item en 1er
+        //check firstItem first
         if (!firstItem) {
-            console.log(`%c🎨 ⍨ firstItem vide`, "Your_CSS_Goes_Here");
+            return
         } else if (firstItem && isFirstItemSignChanged === false){
             isFirstItemSignChanged = true;
-            console.log(`%c🎨 ⍨ isSignChanged`, "color:green; font-weight:bold", isFirstItemSignChanged);
-
             changingSignOfFirstItem();
-            console.log(`%c🎨 ⍨ firstItem modifié`, "Your_CSS_Goes_Here");
         } else if (firstItem && isFirstItemSignChanged === true) {
             isFirstItemSignChanged = false;
-            console.log(`%c🎨 ⍨ isSignChanged`, "color:green; font-weight:bold", isFirstItemSignChanged);
             changingSignOfFirstItem();
         }
     } else {
-        //passer à la vérification de second item en second lieu
+        //then check secondItem 
         if (!secondItem) {
-            console.log(`%c🎨 ⍨ secondItem vide`, "Your_CSS_Goes_Here");
+            return
         } else if (secondItem && isSecondItemSignChanged === false){
             isSecondItemSignChanged = true;
             changingSignOfSecondItem();
-            console.log(`%c🎨 ⍨ secondItem modifié`, "Your_CSS_Goes_Here");
         } else if (secondItem && isSecondItemSignChanged === true) {
             isSecondItemSignChanged = false;
             changingSignOfSecondItem();
@@ -371,15 +359,13 @@ function addOperation() {
         beMinus = false;
         beDivide = false;
         beMultiply = false;
-        //juste coller un moins devant firstItem pas besoin de mettre un autre affichage
-        console.log(`%c🎨 ⍨ reached`, "Your_CSS_Goes_Here");
     } else {
         beAdd  = true;
         beMinus = false;
         beDivide = false;
         beMultiply = false;
         isSecondItem = true;
-    }; // S'assurer que le premier nombre est entré
+    }; // Ensure that the first number is entered
 
 };
 
@@ -392,15 +378,14 @@ function minusOperation() {
         beDivide = false;
         firstItem = firstItem.concat(minusSign, firstItem);
         displayFirstItem.textContent = firstItem;
-        //juste coller un moins devant firstItem pas besoin de mettre un autre affichage
-        console.log(`%c🎨 ⍨ reached`, "Your_CSS_Goes_Here");
+        //just stick a minus in front of firstItem no need to set another display
     } else {
         beMinus = true;
         beAdd = false;
         beMultiply = false;
         beDivide = false;
         isSecondItem = true;
-    }; // S'assurer que le premier nombre est entré
+    }; // Ensure that the first number is entered
 
 
 }
@@ -412,15 +397,13 @@ function multiplyOperation() {
         beMinus = false;
         beDivide = false;
         beMultiply = false;
-        //juste coller un moins devant firstItem pas besoin de mettre un autre affichage
-        console.log(`%c🎨 ⍨ reached`, "Your_CSS_Goes_Here");
     } else {
         beMultiply = true;
         beAdd = false;
         beMinus = false;
         beDivide = false;
         isSecondItem = true;
-    }; // S'assurer que le premier nombre est entré
+    }; // Ensure that the first number is entered
 
 }
 
@@ -432,20 +415,19 @@ function divideOperation() {
         beMinus = false;
         beDivide = false;
         beMultiply = false;
-        //juste coller un moins devant firstItem pas besoin de mettre un autre affichage
-        console.log(`%c🎨 ⍨ reached`, "Your_CSS_Goes_Here");
     } else {
         beDivide = true;
         beMultiply = false;
         beAdd = false;
         beMinus = false;
         isSecondItem = true;
-    }; // S'assurer que le premier nombre est entré
+    }; // Ensure that the first number is entered
 
 
 
 }
 
+//the function that performs the operations 
 function operate(itemOne, itemTwo, operator) {
 
     let results;
@@ -463,7 +445,7 @@ function operate(itemOne, itemTwo, operator) {
 }
 
 
-//KEYBOARD MANAGEMENT
+//KEYBOARD SUPPORT
 
 window.addEventListener(
     "keydown",
@@ -573,6 +555,33 @@ window.addEventListener(
         case "Backspace":
             console.log(`%c🎨 ⍨ backspace`, "Your_CSS_Goes_Here")
             clearOnce();
+            break;
+        case "Enter":
+// console.log(`%c🎨 ⍨ enter`, "Your_CSS_Goes_Here");
+            calculate();
+            break;
+        case ".":
+            restrictionToCommas();
+            break;
+        case "%":
+            restrictionToPercentages();
+            break;
+        case "+":
+            currentOperator = addOperation();
+            displayOperatorsOnScreen(event.key);
+            break;
+        case "-":
+            currentOperator = minusOperation();
+            displayOperatorsOnScreen(event.key);
+            break;
+        case "/":
+            currentOperator = divideOperation();
+            displayOperatorsOnScreen(event.key);
+            break;
+        case "*":
+            currentOperator = multiplyOperation();
+            displayOperatorsOnScreen(event.key);
+            break;
         default:
           return; // Quit when this doesn't handle the key event.
       }
